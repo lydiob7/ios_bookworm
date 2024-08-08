5ff78382c70a76e83cf730902ea54574632158ce
lydiob7 <lydiob7@tutanota.com>
2024-08-08 16:05:31 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tomi Scattini on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
